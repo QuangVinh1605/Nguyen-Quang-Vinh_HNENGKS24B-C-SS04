6775160b29ae58d1f8b77e2d882ce57b32a4b209
QuangVinh1605 <nhv16052006@gmail.com>
2024-11-25 00:56:58 +0700

    upload baitap ss4-01

---
+++ C_SS4-_01.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int number;
+	printf("moi ban nhap vao mot so nguyen");
+	scanf("%d",&number);
+	if(number/2==0) {
+		printf("%d la so chan",number);
+		 
+	}else{
+		printf("%d la so le",number);
+		 
+	} 
+} 
